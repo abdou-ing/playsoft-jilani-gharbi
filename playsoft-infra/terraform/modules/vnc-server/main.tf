0a243b5ec4b613a9b634848cb272a7726b932026
jilanigh <gharbijilani001@gmail.com>
2026-03-04 10:27:25 +0100

    use vm id in main

--- playsoft-infra/terraform/modules/vnc-server/main.tf
+++ playsoft-infra/terraform/modules/vnc-server/main.tf
@@ -11,23 +11,7 @@ module "vm1" {
 
   storage = "local"       
   bridge  = "vmbr0"
+  vm_id   = var.vm_id
 
-  ssh_public_key = "pub key path"
-}
-
-module "vm2" {
-  source = "./modules/vm"
-
-  node_name   = "playsoft-proxmox"
-  vm_name     = "almaLinux-vnc"
-  template_id = 113
-
-  cpu       = 2
-  memory    = 2048
-  disk_size = 20
-
-  storage = "local"       
-  bridge  = "vmbr0"
-
-  ssh_public_key = "pub key path"
+  ssh_public_key = "~/.ssh/jlani.pub"
 }
